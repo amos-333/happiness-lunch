2e10714489cc9e97b1ad694fa08aa783ea742e4c
amos-333 <over2amos333@gmail.com>
2023-03-22 18:19:51 +0530

    group finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      boxShadow: {
+				'full': '0 0px 7px -3px rgba(0, 0, 0, 1)',
+			    },
+		  }
+    },
   plugins: [],
+
 }
