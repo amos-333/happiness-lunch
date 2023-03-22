a42553e1804555e3b939c260282c2f5e4f94fa68
amos-333 <over2amos333@gmail.com>
2023-03-22 20:20:31 +0530

    home with carousel added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 
 @Component({
   selector: 'app-home',
@@ -9,7 +14,26 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const mySwiper = new Swiper('.mySwiper', {
+      effect: "coverflow",
+          grabCursor: true,
+          centeredSlides: true,
+          slidesPerView: "auto",
+          coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 4,
+            slideShadows: true,
+          },
+          rewind:true,
+          autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+          },
+
+    });
   }
 
 }
--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import Swiper from 'swiper';
-// import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle';
-// register Swiper custom elements
-register();
 
 
 @Component({
@@ -16,25 +11,6 @@ export class ResultComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const mySwiper = new Swiper('.mySwiper', {
-      effect: "coverflow",
-          grabCursor: true,
-          centeredSlides: true,
-          slidesPerView: "auto",
-          coverflowEffect: {
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 4,
-            slideShadows: true,
-          },
-          rewind:true,
-          autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-          },
-
-    });
   }
 
 }
