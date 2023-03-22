c1f456f0bbefd8888f05c6a3da88d746caf8a2ba
amos-333 <over2amos333@gmail.com>
2023-03-22 19:32:10 +0530

    option made to black

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import Swiper from 'swiper';
-
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 
 
 @Component({
